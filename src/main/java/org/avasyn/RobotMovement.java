e9f658670c106e048b3b2a4bec48ace8dcd1078f
Fola Akinbobola <voachamber@gmail.com>
2022-10-24 09:30:51 +0100

    refactore RobotMovement class

--- src/main/java/org/avasyn/RobotMovement.java
+++ src/main/java/org/avasyn/RobotMovement.java
@@ -4,7 +4,6 @@ import org.avasyn.exception.RobotMovementException;
 import org.avasyn.simulation.Simulation;
 import org.avasyn.simulation.SquareTable;
 import org.avasyn.simulation.ToyRobot;
-import org.avasyn.simulation.contract.Robot;
 import org.avasyn.simulation.contract.Table;
 import java.util.Scanner;
 
@@ -13,25 +12,20 @@ public class RobotMovement {
     public static void main(String[] args )
     {
         RobotMovement toyRobotMovement = new RobotMovement();
-
-        // create objects
-        Scanner inputPane = new Scanner(System.in);
-        Robot toyRobot = new ToyRobot();
-        Table squareTable = new SquareTable(5, 5);
-        Simulation simulation = new Simulation(squareTable, toyRobot);
-
-        toyRobotMovement.startRobot(simulation,inputPane,toyRobot,squareTable);
+        toyRobotMovement.startRobot();
     }
 
-    private void startRobot( Simulation simulation,Scanner inputPane,Robot toyRobot,Table squareTable) {
+    private void startRobot() {
         //output usage instruction to console
         System.out.println("Enter command in the format Below");
         System.out.println("'PLACE X,Y,[NORTH|SOUTH|EAST|WEST]', EXIT, MOVE, RIGHT, LEFT or REPORT");
 
-        inputPane = new Scanner(System.in);
-        toyRobot = new ToyRobot();
-        squareTable = new SquareTable(5, 5);
-        simulation = new Simulation(squareTable, toyRobot);
+        // create objects
+        Scanner inputPane = new Scanner(System.in);
+        ToyRobot toyRobot = new ToyRobot();
+        Table squareTable = new SquareTable(5, 5);
+        Simulation simulation = new Simulation(squareTable, toyRobot);
+
         boolean isRunning = true;
         while (isRunning) {
             String inputString = inputPane.nextLine();
