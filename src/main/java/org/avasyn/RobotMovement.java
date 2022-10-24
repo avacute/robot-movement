6cf975159508b493d2515780802350c02231a18a
Fola Akinbobola <voachamber@gmail.com>
2022-10-24 09:27:49 +0100

    refactore RobotMovement class

--- src/main/java/org/avasyn/RobotMovement.java
+++ src/main/java/org/avasyn/RobotMovement.java
@@ -4,6 +4,7 @@ import org.avasyn.exception.RobotMovementException;
 import org.avasyn.simulation.Simulation;
 import org.avasyn.simulation.SquareTable;
 import org.avasyn.simulation.ToyRobot;
+import org.avasyn.simulation.contract.Robot;
 import org.avasyn.simulation.contract.Table;
 import java.util.Scanner;
 
@@ -12,20 +13,25 @@ public class RobotMovement {
     public static void main(String[] args )
     {
         RobotMovement toyRobotMovement = new RobotMovement();
-        toyRobotMovement.setTable();
-        toyRobotMovement.startRobot();
-    }
-    private void setTable(){
-        //output usage instruction to console
-        System.out.println("Enter command in the format Below");
-        System.out.println("'PLACE X,Y,[NORTH|SOUTH|EAST|WEST]', EXIT, MOVE, RIGHT, LEFT or REPORT");
-    }
-    private void startRobot() {
+
+        // create objects
         Scanner inputPane = new Scanner(System.in);
-        ToyRobot toyRobot = new ToyRobot();
+        Robot toyRobot = new ToyRobot();
         Table squareTable = new SquareTable(5, 5);
         Simulation simulation = new Simulation(squareTable, toyRobot);
 
+        toyRobotMovement.startRobot(simulation,inputPane,toyRobot,squareTable);
+    }
+
+    private void startRobot( Simulation simulation,Scanner inputPane,Robot toyRobot,Table squareTable) {
+        //output usage instruction to console
+        System.out.println("Enter command in the format Below");
+        System.out.println("'PLACE X,Y,[NORTH|SOUTH|EAST|WEST]', EXIT, MOVE, RIGHT, LEFT or REPORT");
+
+        inputPane = new Scanner(System.in);
+        toyRobot = new ToyRobot();
+        squareTable = new SquareTable(5, 5);
+        simulation = new Simulation(squareTable, toyRobot);
         boolean isRunning = true;
         while (isRunning) {
             String inputString = inputPane.nextLine();
